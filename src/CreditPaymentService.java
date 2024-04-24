424cd9273d62fd5877805d26a8db8fe10cece71f
Ksenia V <vekerle.ksenia@yandex.com>
2024-04-24 09:56:37 +0500

    Correction

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,20 +1,9 @@
 public class CreditPaymentService {
 
-    public double calculate(double i) {
-        double result;
-        result = i / 100 / 12;
-        return result;
-    }
 
-    public int calculate(int n) {
+    public int calculate(int amount, double monthlyPercent, int numberOfPayments) {
         double result;
-        result = n * 12;
-        return (int) result;
-    }
-
-    public int calculate(int S, double monthlyPercent, int term) {
-        double result;
-        result = S * (monthlyPercent + monthlyPercent / (Math.pow((1 + monthlyPercent), term) - 1));
+        result = amount * (monthlyPercent + monthlyPercent / (Math.pow((1 + monthlyPercent), numberOfPayments) - 1));
         return (int) result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,17 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        int S = 1000000; // сумма кредита, руб
-        double i = 9.99; // годовая процентная ставка, %
-        int n = 2; // срок кредита, г
+        int amount = 1000000; // сумма кредита, руб
+        double Percent = 9.99; // годовая процентная ставка, %
+        int term = 2; // срок кредита, г
 
-        double monthlyPercent = service.calculate(i); // ежемесячная процентная ставка
-        int term = service.calculate(n); // Срок кредита
-        int cps = service.calculate(S, monthlyPercent, term); // Ежемесячный платеж
+        double monthlyPercent =  Percent / 100 / 12; // ежемесячная процентная ставка
+        int numberOfPayments = term * 12; // Срок кредита, месяцев
+        int MonthlyPayment = service.calculate(amount, monthlyPercent, numberOfPayments); // Ежемесячный платеж
 
-        System.out.println((S) + "Сумма кредита");
-        System.out.println((term) + "срок кредита, мес");
-        System.out.println((monthlyPercent) + "ежемесячная процентная ставка");
-        System.out.println((cps) + "Ежемесячный платеж");
+        System.out.println((amount) + " руб, сумма кредита");
+        System.out.println((term) + " г, срок кредита");
+        System.out.println((Percent) + " %, процентная ставка");
+        System.out.println((MonthlyPayment) + " руб, ежемесячный платеж");
     }
 }
